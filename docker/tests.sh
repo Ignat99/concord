a54fb60074fe991bb3c1514ff155b1c0ede7381d
Ignat Ignatov <ignat99@gmail.com>
2024-07-21 19:36:38 +0000

    supply-chain

--- docker/tests.sh
+++ docker/tests.sh
@@ -4,15 +4,23 @@
 # SPDX-License-Identifier: Apache-2.0
 
 echo "Pulling samples repo.."
-git clone https://github.com/vmware-samples/vmware-blockchain-samples.git
-cd vmware-blockchain-samples/supply-chain
+# git clone https://github.com/vmware-samples/vmware-blockchain-samples.git
+cd /root/vmware-blockchain-samples/supply-chain
 git checkout tags/v0.1-concord
 
+docker login 192.168.1.101
+
 echo "Building supply chain sample"
-docker build -t supply-chain:latest .
+sudo docker build -t supply-chain:latest .
+
+docker tag supply-chain 192.168.1.101/library/supply-chain:latest
+docker push 192.168.1.101/library/supply-chain:latest
+
+exit 0
 
 echo "Startup Concord"
-cd ../../
+#cd ../../
+cd /root/concord
 docker-compose -f docker/compose/simple4.yml up -d
 
 attempt=0
@@ -35,4 +43,5 @@ while [ $attempt -le 59 ]; do
 done
 
 echo "Running supply chain funtional test"
-docker run --network compose_default -t supply-chain:latest npm run truffle:test:concordDocker
+# docker run --network compose_default -t supply-chain:latest npm run truffle:test:concordDocker
+docker run --network compose_priv-eth-net1 -t supply-chain:latest npm run truffle:test:concordDocker
